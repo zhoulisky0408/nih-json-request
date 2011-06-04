8c4aae230d63f5bcd302f9d1f7602818f468225d
Scott James Remnant <scott@netsplit.com>
2011-06-03 20:26:35 -0700

    Add OAuth body hash code

---
+++ NIHJSONRequest+OAuth.h
@@ -0,0 +1,17 @@
+//
+//  NIHJSONRequest+OAuth.h
+//  CatHerding
+//
+//  Created by Scott James Remnant on 6/3/11.
+//  Copyright 2011 Scott James Remnant <scott@netsplit.com>. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NIHJSONRequest.h"
+#import "ASIHTTPRequest+OAuth.h"
+
+
+@interface NIHJSONRequest (NIHJSONRequest_OAuth)
+
+@end
---
+++ NIHJSONRequest+OAuth.m
@@ -0,0 +1,31 @@
+//
+//  NIHJSONRequest+OAuth.m
+//  CatHerding
+//
+//  Created by Scott James Remnant on 6/3/11.
+//  Copyright 2011 Scott James Remnant <scott@netsplit.com>. All rights reserved.
+//
+
+#import <CommonCrypto/CommonDigest.h>
+
+#import "NSData+Base64.h"
+#import "NIHJSONRequest+OAuth.h"
+
+
+@implementation NIHJSONRequest (NIHJSONRequest_OAuth)
+
+- (NSArray *)oauthAdditionalParametersForMethod:(ASIOAuthSignatureMethod)signatureMethod
+{
+    if (signatureMethod == ASIOAuthPlaintextSignatureMethod)
+        return nil;
+
+    unsigned char digestBytes[CC_SHA1_DIGEST_LENGTH];    
+    CC_SHA1([[self postBody] bytes], [[self postBody] length], digestBytes);
+
+    NSData *digestData = [NSData dataWithBytes:digestBytes length:CC_SHA1_DIGEST_LENGTH];
+    NSString *digest = [digestData base64EncodedString];
+
+    return [NSArray arrayWithObject:[NSDictionary dictionaryWithObjectsAndKeys:@"oauth_body_hash", @"key", digest, @"value", nil]];
+}
+
+@end
